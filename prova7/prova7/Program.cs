75e23a2f430948be204517024ebbe4fd58ff08ac
ANA JULLYA MIRANDA DOS SANTOS <anajullyamirandadossantos@gmail.com>
2025-10-17 16:27:26 -0300

    Add files via upload

---
+++ prova7/prova7/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Digite um número inicial:");
+int numeroinicial = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Agora digite um número final:");
+int numerofinal = int.Parse(Console.ReadLine());
+
+if (numeroinicial > numerofinal)
+{
+    Console.WriteLine("O número inicial não pode ser menor do que o final");
+    int temp = numeroinicial;
+    numeroinicial = numerofinal;
+    numerofinal = temp;
+}
+Console.WriteLine();
+
+for (int i = numeroinicial; i <= numerofinal; i++)
+{
+    Console.WriteLine($"Tabuada do {i}:"); 
+
+    for (int j = 1; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine(); 
+}
+
+
+
+
+
